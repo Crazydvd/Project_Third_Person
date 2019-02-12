7fa6b66a0ad79b074ed7685d4abcff606f608a68
Danny van Duist <dvduist@hotmail.nl>
2019-02-12 11:49:46 +0100

    Added the engine.

---
+++ mge_v18_student_version/src/ThirdPerson/TPerson.hpp
@@ -0,0 +1,38 @@
+#ifndef TPERSON_HPP
+#define TPERSON_HPP
+
+#include "mge/core/AbstractGame.hpp"
+
+class DebugHud;
+
+/**
+ * An example subclass of AbstractGame showing how we can setup a scene.
+ */
+class TPerson: public AbstractGame
+{
+    //PUBLIC FUNCTIONS
+
+	public:
+		TPerson();
+		virtual ~TPerson();
+
+        //override initialize so we can add a DebugHud
+        virtual void initialize();
+
+	protected:
+	    //override so we can construct the actual scene
+        virtual void _initializeScene();
+
+	    //override render to render the hud as well.
+	    virtual void _render();
+
+	private:
+		DebugHud* _hud;                   //hud display
+
+        void _updateHud();
+
+        TPerson(const TPerson&);
+        TPerson& operator=(const TPerson&);
+};
+
+#endif // TPERSON_HPP
---
+++ mge_v18_student_version/src/ThirdPerson/config.hpp
@@ -0,0 +1,7 @@
+namespace config {
+    #include <string>
+    const std::string THIRDPERSON_MODEL_PATH ("ThirdPerson/models/");
+    const std::string THIRDPERSON_TEXTURE_PATH ("ThirdPerson/textures/");
+    const std::string THIRDPERSON_SHADER_PATH ("ThirdPerson/shaders/");
+    const std::string THIRDPERSON_FONT_PATH ("ThirdPerson/fonts/");
+}
