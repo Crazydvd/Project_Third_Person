1bb6763ef03cd15ef2436ff8accd27f6b5722d6d
Danny van Duist <dvduist@hotmail.nl>
2019-02-12 15:07:16 +0100

    some small reformatting.

--- mge_v18_student_version/src/ThirdPerson/TPerson.hpp
+++ mge_v18_student_version/src/ThirdPerson/TPerson.hpp
@@ -8,31 +8,31 @@ class DebugHud;
 /**
  * An example subclass of AbstractGame showing how we can setup a scene.
  */
-class TPerson: public AbstractGame
+class TPerson : public AbstractGame
 {
-    //PUBLIC FUNCTIONS
+	//PUBLIC FUNCTIONS
 
-	public:
-		TPerson();
-		virtual ~TPerson();
+public:
+	TPerson();
+	virtual ~TPerson();
 
-        //override initialize so we can add a DebugHud
-        virtual void initialize();
+	//override initialize so we can add a DebugHud
+	virtual void initialize();
 
-	protected:
-	    //override so we can construct the actual scene
-        virtual void _initializeScene();
+protected:
+	//override so we can construct the actual scene
+	virtual void _initializeScene();
 
-	    //override render to render the hud as well.
-	    virtual void _render();
+	//override render to render the hud as well.
+	virtual void _render();
 
-	private:
-		DebugHud* _hud;                   //hud display
+private:
+	DebugHud* _hud;                   //hud display
 
-        void _updateHud();
+	void _updateHud();
 
-        TPerson(const TPerson&);
-        TPerson& operator=(const TPerson&);
+	TPerson(const TPerson&);
+	TPerson& operator=(const TPerson&);
 };
 
 #endif // TPERSON_HPP
